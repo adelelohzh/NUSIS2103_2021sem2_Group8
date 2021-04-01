f0cc9e0a4aaa9cbc73a9b50dd68ef5e3ae711523
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-01 11:12:46 +0800

    edited appointmententitysessionbean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBean.java
@@ -3,13 +3,17 @@ package ejb.session.stateless;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 
 @Stateless
 @Local(AppointmentEntitySessionBeanLocal.class)
 @Remote(AppointmentEntitySessionBeanRemote.class)
-public class AppointmentEntitySessionBean implements AppointmentEntitySessionBeanRemote, AppointmentEntitySessionBeanLocal {
+public class AppointmentEntitySessionBean implements AppointmentEntitySessionBeanRemote, AppointmentEntitySessionBeanLocal 
+{
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "EasyAppointmentSystemWeb-ejbPU")
+    private EntityManager em;
+    
 }
