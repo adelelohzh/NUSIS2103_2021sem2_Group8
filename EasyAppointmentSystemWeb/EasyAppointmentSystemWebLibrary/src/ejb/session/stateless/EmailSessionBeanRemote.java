a111aca2c606a2ec8971a39e9d7644519a48780a
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-01 21:39:46 +0800

    edited emailsessionbeanremote

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/EmailSessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/EmailSessionBeanRemote.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejb.session.stateless;
 
 import javax.ejb.Remote;
 
-/**
- *
- * @author jieyingma
- */
-@Remote
-public interface EmailSessionBeanRemote {
+
+public interface EmailSessionBeanRemote 
+{
     
 }
