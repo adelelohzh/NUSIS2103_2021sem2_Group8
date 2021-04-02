ec3530a3f86e62fbeb42ce36a8f766f311b49ab6
valenciateh <valenciateh@tehs-mbp>
2021-04-02 14:21:36 +0800

    edited CustomerEntitySessionBeanRemote

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/CustomerEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/CustomerEntitySessionBeanRemote.java
@@ -1,5 +1,10 @@
 package ejb.session.stateless;
 
+import entity.CustomerEntity;
+import util.exception.CustomerNotFoundException;
+
 public interface CustomerEntitySessionBeanRemote {
     
+     public CustomerEntity retrieveCustomerEntityByCustomerId(Long customerId) throws CustomerNotFoundException;
+     
 }
