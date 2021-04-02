55b7000ba21f3b8943d83c27b68b93b74f149bea
valenciateh <valenciateh@tehs-mbp>
2021-04-02 20:34:11 +0800

    edited Appointment EntitySessionBeanLocal

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBeanLocal.java
@@ -1,6 +1,11 @@
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
+import util.exception.AppointmentNotFoundException;
+
 
 public interface AppointmentEntitySessionBeanLocal {
+
+    public AppointmentEntity retrieveAppointmentByCustomerID(Long customerID) throws AppointmentNotFoundException;
     
 }
