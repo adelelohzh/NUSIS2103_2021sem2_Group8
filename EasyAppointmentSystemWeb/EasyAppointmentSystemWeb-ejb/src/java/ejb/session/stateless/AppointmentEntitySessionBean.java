f52794443eb980a062a9d9beb8531f1f523c21ba
valenciateh <valenciateh@tehs-mbp>
2021-04-02 20:34:48 +0800

    edited AppointmentEntitySessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBean.java
@@ -1,10 +1,15 @@
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.AppointmentNotFoundException;
 
 
 @Stateless
@@ -16,4 +21,18 @@ public class AppointmentEntitySessionBean implements AppointmentEntitySessionBea
     @PersistenceContext(unitName = "EasyAppointmentSystemWeb-ejbPU")
     private EntityManager em;
     
+    public AppointmentEntity retrieveAppointmentByCustomerID(Long customerID) throws AppointmentNotFoundException {
+   
+        Query query = em.createQuery("SELECT a FROM AppointmentEntity a WHERE a.customerID = :inCustomerID");
+        query.setParameter("inCustomerID", customerID);
+        
+        try
+        {
+            return (AppointmentEntity)query.getSingleResult();
+        }
+        catch(NoResultException | NonUniqueResultException ex)
+        {
+            throw new AppointmentNotFoundException("Appointment of customer " + customerID + " does not exist!");
+        }
+    }
 }
