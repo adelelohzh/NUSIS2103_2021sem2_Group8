60e052ec116d867bc81e163d0f7a7c401e057716
valenciateh <valenciateh@tehs-mbp>
2021-04-03 00:22:34 +0800

    edited BusinessCategoryEntitySessionBeanLocal

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/BusinessCategoryEntitySessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/BusinessCategoryEntitySessionBeanLocal.java
@@ -1,6 +1,11 @@
 package ejb.session.stateless;
 
+import entity.BusinessCategoryEntity;
+import java.util.List;
+
 
 public interface BusinessCategoryEntitySessionBeanLocal {
     
+     public List<BusinessCategoryEntity> retrieveAllBusinessCategories();
+
 }
