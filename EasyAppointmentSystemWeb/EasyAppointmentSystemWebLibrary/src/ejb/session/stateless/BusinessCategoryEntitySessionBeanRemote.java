3b344f85472e29be37f19895797f9b8eef07596c
valenciateh <valenciateh@tehs-mbp>
2021-04-03 00:21:50 +0800

    edited BusinessCategoryEntitySessionBeanRemote

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
@@ -1,6 +1,10 @@
 package ejb.session.stateless;
 
+import entity.BusinessCategoryEntity;
+import java.util.List;
+
 
 public interface BusinessCategoryEntitySessionBeanRemote {
     
+     public List<BusinessCategoryEntity> retrieveAllBusinessCategories();
 }
