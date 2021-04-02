6f392b3a9e901650e89418627fda0efb1eccdc91
valenciateh <valenciateh@tehs-mbp>
2021-04-03 00:39:11 +0800

    edited BusinessCategoryEntitySessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
@@ -2,9 +2,12 @@ package ejb.session.stateless;
 
 import entity.BusinessCategoryEntity;
 import java.util.List;
+import util.exception.CreateNewBusinessCategoryException;
 
 
 public interface BusinessCategoryEntitySessionBeanRemote {
     
      public List<BusinessCategoryEntity> retrieveAllBusinessCategories();
+
+    public BusinessCategoryEntity createNewBusinessCategoryEntity(BusinessCategoryEntity newBusinessCategoryEntity) throws CreateNewBusinessCategoryException;
 }
