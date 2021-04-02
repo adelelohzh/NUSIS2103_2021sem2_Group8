d8282e36277487655a708e7b8483917c4644a39f
valenciateh <valenciateh@tehs-mbp>
2021-04-02 14:22:02 +0800

    edited CustomerEntitySessionBeanLocal

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/CustomerEntitySessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/CustomerEntitySessionBeanLocal.java
@@ -1,6 +1,11 @@
 package ejb.session.stateless;
 
+import entity.CustomerEntity;
+import util.exception.CustomerNotFoundException;
+
 
 public interface CustomerEntitySessionBeanLocal {
+
+    public CustomerEntity retrieveCustomerEntityByCustomerId(Long customerId) throws CustomerNotFoundException;
     
 }
