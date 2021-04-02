15482a5ee7b39dd969f776a92bdca6e8a754f624
valenciateh <valenciateh@tehs-mbp>
2021-04-02 11:28:31 +0800

    edited AdminEntitySessionBeanRemote

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AdminEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AdminEntitySessionBeanRemote.java
@@ -1,5 +1,13 @@
 package ejb.session.stateless;
 
+import entity.AdminEntity;
+import java.util.List;
+import util.exception.InvalidLoginCredentialException;
+
 public interface AdminEntitySessionBeanRemote {
+
+    public List<AdminEntity> retrieveAllAdmins();
+
+    public AdminEntity adminLogin(String username, String password) throws InvalidLoginCredentialException;;
     
 }
