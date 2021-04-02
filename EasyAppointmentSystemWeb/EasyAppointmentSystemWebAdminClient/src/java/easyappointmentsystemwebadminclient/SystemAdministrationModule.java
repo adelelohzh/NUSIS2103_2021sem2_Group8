5e77a5cd3c223e2ecd165e4fdf4e6c8c4b1f4340
valenciateh <valenciateh@tehs-mbp>
2021-04-02 14:46:47 +0800

    edited SystemAdministrationModule

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebAdminClient/src/java/easyappointmentsystemwebadminclient/SystemAdministrationModule.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebAdminClient/src/java/easyappointmentsystemwebadminclient/SystemAdministrationModule.java
@@ -3,10 +3,16 @@ package easyappointmentsystemwebadminclient;
 import ejb.session.stateless.CustomerEntitySessionBeanRemote;
 import ejb.session.stateless.ServiceProviderEntitySessionBeanRemote;
 import entity.AdminEntity;
+import entity.CustomerEntity;
+import entity.ServiceProviderEntity;
+import java.util.List;
+import java.util.Scanner;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import util.exception.CustomerNotFoundException;
 import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderNotFoundException;
 import util.exception.UnknownPersistenceException;
 
 
@@ -19,7 +25,6 @@ public class SystemAdministrationModule {
     private ServiceProviderEntitySessionBeanRemote serviceProviderEntitySessionBeanRemote;
     
     private AdminEntity currentAdminEntity;
-
     
     
     public SystemAdministrationModule()
@@ -27,8 +32,6 @@ public class SystemAdministrationModule {
         validatorFactory = Validation.buildDefaultValidatorFactory();
         validator = validatorFactory.getValidator();
     }
-
-    
     
     public SystemAdministrationModule(CustomerEntitySessionBeanRemote customerEntitySessionBeanRemote, ServiceProviderEntitySessionBeanRemote serviceProviderEntitySessionBeanRemote, AdminEntity currentAdminEntity) 
     {
@@ -38,35 +41,88 @@ public class SystemAdministrationModule {
         this.currentAdminEntity = currentAdminEntity;
     }
 
-    void viewCustomerAppointments() {
+    public void viewCustomerAppointments() {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*** Admin terminal :: View Appointments for customers ***\n");
+        System.out.print("Enter customer Id> ");
+        
+        Long customerId = sc.nextLong();
+        
+        try {
+            CustomerEntity customerEntity = customerEntitySessionBeanRemote.retrieveCustomerEntityByCustomerId(customerId);
+            //retrieve appointments
+            //print appointments
+            System.out.println("Enter 0 to go back to the previous menu.");
+            
+            System.out.print("Enter customer Id> ");
+            Long input = sc.nextLong();   
+        }  
+        catch(CustomerNotFoundException ex)
+        {
+            System.out.println("An error has occurred while retrieving customer: " + ex.getMessage() + "\n");
+        }
+    }
+
+    public void viewServiceProviderAppointments() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*** Admin terminal :: View Appointments for service providers ***\n");
+        System.out.print("Enter service provider Id> ");
+        
+        Long serviceProviderId = sc.nextLong();
+        
+        try {
+            ServiceProviderEntity serviceProviderEntity = serviceProviderEntitySessionBeanRemote.retrieveServiceProviderEntityById(serviceProviderId);
+            //retrieve appointments
+            //print appointments
+            System.out.println("Enter 0 to go back to the previous menu.");
+            
+            System.out.print("Enter service provider Id> ");
+            Long input = sc.nextLong();   
+        }
+        catch (ServiceProviderNotFoundException ex)
+        {
+            System.out.println("An error has occurred while retrieving service provider: " + ex.getMessage() + "\n");
+        }
+    }
+
+    public void viewServiceProviders() {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*** Admin terminal :: View service providers ***\n");
+        System.out.println("*** POS System :: System Administration :: View All Service Providers ***\n");
+        
+        List<ServiceProviderEntity> serviceProviderEntities = serviceProviderEntitySessionBeanRemote.retrieveAllServiceProviderEntity();
+        //System.out.printf("%8s%20s%20s%15s%20s%20s\n", "Id | ", "Name | ", "Business Category | ", "Business Reg. No. | ", "City | ", " Address | ", " Email | ", "Phone");
+
+        for(ServiceProviderEntity serviceProviderEntity:serviceProviderEntities)
+        {
+            //System.out.printf("%8s%20s%20s%15s%20s%20s\n", serviceProviderEntity.getServiceProviderId().toString(), serviceProviderEntity.getName(), staffEntity.getPassword());
+        }
+        
+        System.out.print("Press any key to continue...> ");
+        sc.nextLine();
+    
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void approveServiceProviders() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void viewServiceProviderAppointments() {
+    public void blockServiceProviders() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void viewServiceProviders() {
+    public void addBusinessCategory() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void approveServiceProviders() {
+    public void removeBusinessCategory() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void blockServiceProviders() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void addBusinessCategory() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void removeBusinessCategory() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void sendReminderEmail() {
+    public void sendReminderEmail() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
