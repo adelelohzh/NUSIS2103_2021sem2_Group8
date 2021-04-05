1579024d96f400c60b09616420158c34ce255fd3
valenciateh <valenciateh@tehs-mbp>
2021-04-06 00:25:23 +0800

    edited EmailSessionBeanLocal

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/EmailSessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/EmailSessionBeanLocal.java
@@ -1,6 +1,14 @@
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
+import java.util.List;
+import java.util.concurrent.Future;
+
 
 public interface EmailSessionBeanLocal {
+    
+    public Boolean emailCheckoutNotificationSync(List<AppointmentEntity> customerAppointmentEntities, String name_namecompnusedusg, String toEmailAddress);
+
+    public Future<Boolean> emailCheckoutNotificationAsync(List<AppointmentEntity> appointmentEntities, String fromEmailAddress, String toEmailAddress) throws InterruptedException;
     
 }
