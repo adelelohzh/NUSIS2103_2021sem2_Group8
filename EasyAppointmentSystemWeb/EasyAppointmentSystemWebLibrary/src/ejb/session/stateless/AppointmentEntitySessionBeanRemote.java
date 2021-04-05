7683c62e8afd9e5055f56f3e7a0f8c65e587b40e
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-05 12:03:41 +0800

    edited appointmentEntitySessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
@@ -6,4 +6,8 @@ import util.exception.AppointmentNotFoundException;
 public interface AppointmentEntitySessionBeanRemote {
     
     public AppointmentEntity retrieveAppointmentByCustomerID(Long customerID) throws AppointmentNotFoundException;
+    
+    public AppointmentEntity retrieveAppointmentByAppointmentNumber(String appointmentNo) throws AppointmentNotFoundException;
+
+    public void deleteAppointment(String appointmentNo) throws AppointmentNotFoundException;
 }
