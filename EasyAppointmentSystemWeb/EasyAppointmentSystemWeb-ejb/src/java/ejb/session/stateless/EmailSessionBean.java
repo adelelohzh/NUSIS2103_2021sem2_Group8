cfc148e4b8a1dbca265e3eeb182a242effdbd9bd
valenciateh <valenciateh@tehs-mbp>
2021-04-06 00:25:03 +0800

    edited EmailSessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/EmailSessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/EmailSessionBean.java
@@ -1,23 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
+import java.util.List;
+import java.util.concurrent.Future;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import util.email.EmailManager;
 
-/**
- *
- * @author jieyingma
- */
+
+
 @Stateless
 @Local(EmailSessionBeanLocal.class)
 @Remote(EmailSessionBeanRemote.class)
-public class EmailSessionBean implements EmailSessionBeanRemote, EmailSessionBeanLocal {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+public class EmailSessionBean implements EmailSessionBeanLocal, EmailSessionBeanRemote 
+{
+    private final String GMAIL_USERNAME = "<REPLACE_WITH_GMAIL_EMAIL>";
+    private final String GMAIL_PASSWORD = "<REPLACE_WITH_GMAIL_PASSWORD>";
+    
+    
+    @Override
+    public Boolean emailCheckoutNotificationSync(List<AppointmentEntity> appointmentEntities, String fromEmailAddress, String toEmailAddress)
+    {
+        EmailManager emailManager = new EmailManager(GMAIL_USERNAME, GMAIL_PASSWORD);
+        Boolean result = emailManager.emailCheckoutNotification(appointmentEntities, fromEmailAddress, toEmailAddress);
+        
+        return result;
+    } 
+    
+    
+    
+    @Asynchronous
+    public Future<Boolean> emailCheckoutNotificationAsync(List<AppointmentEntity> appointmentEntities, String fromEmailAddress, String toEmailAddress) throws InterruptedException
+    {        
+        EmailManager emailManager = new EmailManager(GMAIL_USERNAME, GMAIL_PASSWORD);
+        Boolean result = emailManager.emailCheckoutNotification(appointmentEntities, fromEmailAddress, toEmailAddress);
+        
+        return new AsyncResult<>(result);
+    }
 }
