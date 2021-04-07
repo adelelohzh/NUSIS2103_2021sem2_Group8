b0bd3901806e5171e649b2d044872c6d23f653f9
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-07 21:43:37 +0800

    edited appointment and serviceProvidersessionbean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
@@ -1,6 +1,8 @@
 package ejb.session.stateless;
 
 import entity.AppointmentEntity;
+import java.util.Date;
+import java.util.List;
 import util.exception.AppointmentNotFoundException;
 
 public interface AppointmentEntitySessionBeanRemote {
@@ -10,4 +12,6 @@ public interface AppointmentEntitySessionBeanRemote {
     public AppointmentEntity retrieveAppointmentByAppointmentNumber(String appointmentNo) throws AppointmentNotFoundException;
 
     public void deleteAppointment(String appointmentNo) throws AppointmentNotFoundException;
+    
+    public List<AppointmentEntity> retrieveAppointmentsByDate(Date date, String serviceProviderName);
 }
