2a6690982b4925547b8cf74d8194bba7fa1d2b0c
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-07 21:42:54 +0800

    edited customerclient

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebCustomerClient/src/java/easyappointmentsystemwebcustomerclient/SystemAdministrationModule.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebCustomerClient/src/java/easyappointmentsystemwebcustomerclient/SystemAdministrationModule.java
@@ -11,11 +11,16 @@ import ejb.session.stateless.EmailSessionBeanRemote;
 import ejb.session.stateless.ServiceProviderEntitySessionBeanRemote;
 import entity.AdminEntity;
 import entity.CustomerEntity;
+import entity.ServiceProviderEntity;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import util.exception.ServiceProviderNotFoundException;
 
 /**
  *
@@ -48,6 +53,34 @@ public class SystemAdministrationModule
         this.currentAdminEntity = currentAdminEntity;
         this.queueCheckoutNotification = queueCheckoutNotification;
         this.queueCheckoutNotificationFactory = queueCheckoutNotificationFactory;
+    }
+    
+    public void searchOperation(String businessCategrory, Date date, String city)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*** Customer terminal :: Search Operation ***\n");
+        String response = "";
+        
+        try
+        {
+            List<ServiceProviderEntity> serviceProviders = serviceProviderEntitySessionBeanRemote.retrieveServiceProviderEntityBySearch(businessCategrory, date, city);
+            System.out.printf("%-19s%-6s%-22s%-9s%-16s\n", "Service Provider Id", "| Name", "| First available Time", "| Address", "| Overall rating");
+            for (ServiceProviderEntity s: serviceProviders)
+            {
+                //find earliest time
+                System.out.printf("%-19s%-6s%-22s%-9s%-16s\n", s.getServiceProviderId(), "| " + s.getName(), "| " + time, "| " + s.getBusinessAddress(), "| " + s.getRating());
+            }
+        }
+        catch(ServiceProviderNotFoundException ex)
+        {
+            System.out.println("Service Provider cannot be found!");
+        }
+            
+        while (response != "0")
+        {
+            System.out.println("Enter 0 to go back to the previous menu.");
+            System.out.print("Exit> ");
+        }
     }
     
 }
