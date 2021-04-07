213204e0f7f7f4b4d6dbd9478fca2d0fdfa4b678
valenciateh <valenciateh@tehs-mbp>
2021-04-07 14:14:50 +0800

    edited CustomerEntitySessionBeanLocal

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/CustomerEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/CustomerEntitySessionBeanRemote.java
@@ -1,10 +1,23 @@
 package ejb.session.stateless;
 
 import entity.CustomerEntity;
+import java.util.List;
 import util.exception.CustomerNotFoundException;
+import util.exception.CustomerUsernameExistException;
+import util.exception.InputDataValidationException;
+import util.exception.InvalidLoginCredentialException;
+import util.exception.UnknownPersistenceException;
 
 public interface CustomerEntitySessionBeanRemote {
-    
-     public CustomerEntity retrieveCustomerEntityByCustomerId(Long customerId) throws CustomerNotFoundException;
-     
+
+    public CustomerEntity retrieveCustomerEntityByCustomerId(Long customerId) throws CustomerNotFoundException;
+
+    public Long createNewCustomer(CustomerEntity newCustomerEntity) throws CustomerUsernameExistException, UnknownPersistenceException, InputDataValidationException;
+
+    public CustomerEntity retrieveCustomerByEmail(String email) throws CustomerNotFoundException;
+
+    public List<CustomerEntity> retrieveAllCustomers();
+
+    public CustomerEntity customerLogin(String email, String password) throws InvalidLoginCredentialException;
+
 }
