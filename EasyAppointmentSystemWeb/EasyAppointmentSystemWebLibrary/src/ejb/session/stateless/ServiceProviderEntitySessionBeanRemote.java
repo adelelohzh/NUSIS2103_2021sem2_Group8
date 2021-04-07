73e61bc03894e158a595515f788e04ad690c3c60
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-07 21:16:50 +0800

    edited serviceProvider sessionbean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/ServiceProviderEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/ServiceProviderEntitySessionBeanRemote.java
@@ -1,6 +1,7 @@
 package ejb.session.stateless;
 
 import entity.ServiceProviderEntity;
+import java.util.Date;
 import java.util.List;
 import util.exception.DeleteServiceProviderException;
 import util.exception.InputDataValidationException;
@@ -28,5 +29,7 @@ public interface ServiceProviderEntitySessionBeanRemote
 
     public void registerServiceProvider(String name, int category, String businessRegNumber, String city, String phone, String addr, String email, String password) throws ServiceProviderEmailExistException, UnknownPersistenceException, InputDataValidationException;
     
+    public List<ServiceProviderEntity> retrieveServiceProviderEntityBySearch(String businessCategory, Date date, String city) throws ServiceProviderNotFoundException;
+
     
 }
