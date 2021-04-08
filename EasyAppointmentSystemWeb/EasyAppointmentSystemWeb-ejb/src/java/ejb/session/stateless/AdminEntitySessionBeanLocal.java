29e464cfd3fcac27cded9b01427a143b4192856c
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-04-08 23:44:10 +0800

    edited AdminEntitySessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AdminEntitySessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateless/AdminEntitySessionBeanLocal.java
@@ -3,7 +3,10 @@ package ejb.session.stateless;
 import entity.AdminEntity;
 import java.util.List;
 import util.exception.AdminNotFoundException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InputDataValidationException;
 import util.exception.InvalidLoginCredentialException;
+import util.exception.UpdateAdminException;
 
 public interface AdminEntitySessionBeanLocal {
     
@@ -12,5 +15,11 @@ public interface AdminEntitySessionBeanLocal {
     public AdminEntity adminLogin(String email, String password) throws InvalidLoginCredentialException;
 
     public AdminEntity retrieveAdminByEmail(String email) throws AdminNotFoundException;
+
+    public AdminEntity retrieveAdminEntityById(Long adminId) throws AdminNotFoundException;
+
+    public void updateAdminEntity(AdminEntity adminEntity) throws InputDataValidationException, UpdateAdminException, AdminNotFoundException;
+
+    public void deleteAdmin(Long adminId) throws AdminNotFoundException;
     
 }
--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AdminEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/AdminEntitySessionBeanRemote.java
@@ -3,7 +3,10 @@ package ejb.session.stateless;
 import entity.AdminEntity;
 import java.util.List;
 import util.exception.AdminNotFoundException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InputDataValidationException;
 import util.exception.InvalidLoginCredentialException;
+import util.exception.UpdateAdminException;
 
 public interface AdminEntitySessionBeanRemote {
 
@@ -12,5 +15,11 @@ public interface AdminEntitySessionBeanRemote {
     public AdminEntity adminLogin(String username, String password) throws InvalidLoginCredentialException;
     
     public AdminEntity retrieveAdminByEmail(String email) throws AdminNotFoundException;
+    
+    public AdminEntity retrieveAdminEntityById(Long adminId) throws AdminNotFoundException;
+
+    public void updateAdminEntity(AdminEntity adminEntity) throws InputDataValidationException, UpdateAdminException, AdminNotFoundException;
+
+    public void deleteAdmin(Long adminId) throws AdminNotFoundException;
     
 }
