ffd50b78fe279ecd2f5e8a17a324717a5fcac267
valenciateh <valenciateh@tehs-mbp>
2021-04-09 02:44:30 +0800

    edited DataInitSB

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
@@ -50,7 +50,7 @@ public class DataInitializationSessionBean {
     {
         try
         {
-            adminEntitySessionBeanLocal.retrieveAdminEntityByName("Admin01");
+            adminEntitySessionBeanLocal.retrieveAdminEntityById(1L);
         }
         catch(AdminNotFoundException ex)
         {
