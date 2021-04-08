322b3cafbedd5f0effcd2d7d49c83b505b76fdd0
valenciateh <valenciateh@tehs-mbp>
2021-04-08 20:39:26 +0800

    edited BookingSessionBean

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBean.java
@@ -1,10 +1,22 @@
 package ejb.session.stateful;
 
+import ejb.session.stateless.AppointmentEntitySessionBeanLocal;
+import entity.AppointmentEntity;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Remote;
 import javax.ejb.Stateful;
 
 
 @Stateful
-public class BookingSessionBean implements BookingSessionBeanRemote, BookingSessionBeanLocal {
+@Local(BookingSessionBeanLocal.class)
+@Remote(BookingSessionBeanRemote.class)
+public class BookingSessionBean implements BookingSessionBeanLocal, BookingSessionBeanRemote {
 
-   
+    @EJB
+    private AppointmentEntitySessionBeanLocal appointmentEntitySessionBeanLocal;
+    
+    private List<AppointmentEntity> appointmentEntities;
+    
 }
