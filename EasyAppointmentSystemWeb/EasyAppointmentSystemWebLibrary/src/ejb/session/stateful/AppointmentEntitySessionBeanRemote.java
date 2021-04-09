babe5ed3f46b607116127d5feead4ec1af158914
valenciateh <valenciateh@tehs-mbp>
2021-04-10 03:11:40 +0800

    edited AppointmentEntitySessionBeanRemote

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateful/AppointmentEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateful/AppointmentEntitySessionBeanRemote.java
@@ -25,4 +25,6 @@ public interface AppointmentEntitySessionBeanRemote {
     
     public AppointmentEntity retrieveAppointmentByAppointmentNumber(String appointmentNo) throws AppointmentNotFoundException;
     
+    public void updateAppointmentEntity(AppointmentEntity appointmentEntity);
+    
 }
