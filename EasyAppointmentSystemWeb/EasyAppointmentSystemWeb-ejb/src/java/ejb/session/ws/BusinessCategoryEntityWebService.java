f2ca5767bd3a74e2f7295136c1c1ba3b5f295d95
jieyingma <jieyingma@jieyings-mbp.lan>
2021-04-12 00:50:28 +0800

    commit businessWebService

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/ws/BusinessCategoryEntityWebService.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/ws/BusinessCategoryEntityWebService.java
@@ -10,11 +10,13 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import util.exception.BusinessCategoryNotFoundException;
+import util.exception.DeleteBusinessCategoryException;
 import util.exception.InvalidLoginCredentialException;
 
 
 @WebService(serviceName = "BusinessCategoryEntityWebService")
-@Stateless()
+@Stateless
 public class BusinessCategoryEntityWebService 
 {
 
@@ -37,5 +39,44 @@ public class BusinessCategoryEntityWebService
         
         return businessCategoryEntitySessionBeanLocal.retrieveAllBusinessCategories();
     }
-            
+    
+    @WebMethod(operationName = "retrieveBusinessCategoriesByName")
+    public BusinessCategoryEntity retrieveBusinessCategoriesByName(@WebParam(name = "email") String emailAddr,
+                                                                         @WebParam(name = "password") String password,
+                                                                         @WebParam(name = "businessCategoryName") String businessCategoryName)                                                      
+                                                            throws InvalidLoginCredentialException, BusinessCategoryNotFoundException
+    {
+        CustomerEntity customerEntity = customerEntitySessionBeanLocal.customerLogin(emailAddr, password);
+        System.out.println("********** EasyAppointmentSystem.retrieveBusinessCategoriesByName(): Customer " 
+                            + customerEntity.getEmailAddress() 
+                            + " login remotely via web service");
+        
+        return businessCategoryEntitySessionBeanLocal.retrieveBusinessCategoriesByName(businessCategoryName);
+    }
+    
+    @WebMethod(operationName = "retrieveBusinessCategoriesById")
+    public BusinessCategoryEntity retrieveBusinessCategoriesById(@WebParam(name = "email") String emailAddr,
+                                                                 @WebParam(name = "password") String password,
+                                                                 @WebParam(name = "businessCategoryId") Long businessCategoryId)                                                      
+                                                            throws InvalidLoginCredentialException, BusinessCategoryNotFoundException
+    {
+        CustomerEntity customerEntity = customerEntitySessionBeanLocal.customerLogin(emailAddr, password);
+        System.out.println("********** EasyAppointmentSystem.retrieveBusinessCategoriesById(): Customer " 
+                            + customerEntity.getEmailAddress() 
+                            + " login remotely via web service");
+        
+        return businessCategoryEntitySessionBeanLocal.retrieveBusinessCategoriesById(businessCategoryId);
+    }
+    
+    @WebMethod(operationName = "deleteBusinessCategory")
+    public void deleteBusinessCategory(@WebParam(name = "email") String emailAddr,
+                                       @WebParam(name = "password") String password,
+                                       @WebParam(name = "businessCategoryId") Long businessCategoryId)                                                      
+                                throws InvalidLoginCredentialException, BusinessCategoryNotFoundException, DeleteBusinessCategoryException
+    {
+        CustomerEntity customerEntity = customerEntitySessionBeanLocal.customerLogin(emailAddr, password);
+        System.out.println("********** EasyAppointmentSystem.deleteBusinessCategory(): Customer " 
+                            + customerEntity.getEmailAddress() 
+                            + " login remotely via web service");
+    }       
 }
