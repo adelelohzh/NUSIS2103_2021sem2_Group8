d95f3cebdf2d770e41de4d5a9c6ecedc7f228487
jieyingma <jieyingma@jieyings-mbp.lan>
2021-04-11 19:06:24 +0800

    commit bookingSB

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBean.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBean.java
@@ -2,7 +2,12 @@ package ejb.session.stateful;
 
 import ejb.session.stateless.AppointmentEntitySessionBeanLocal;
 import entity.AppointmentEntity;
+import entity.BusinessCategoryEntity;
+import entity.CustomerEntity;
+import entity.ServiceProviderEntity;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
@@ -10,6 +15,12 @@ import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
+import util.exception.AppointmentNumberExistsException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderBlockedException;
+import util.exception.ServiceProviderNotFoundException;
+import util.exception.UnknownPersistenceException;
 
 
 @Stateful
@@ -20,8 +31,14 @@ public class BookingSessionBean implements BookingSessionBeanRemote, BookingSess
 
     @EJB
     private AppointmentEntitySessionBeanLocal appointmentEntitySessionBeanLocal;
-   
-    private Integer totalQuantity;    
+    
+    private String appointmentNo;
+    private LocalTime scheduledTime;
+    private LocalDate scheduledDate;
+    private CustomerEntity customerEntity;
+    private ServiceProviderEntity serviceProviderEntity;
+    private BusinessCategoryEntity businessCategoryEntity;
+      
     
     public BookingSessionBean() 
     {
@@ -39,44 +56,88 @@ public class BookingSessionBean implements BookingSessionBeanRemote, BookingSess
     
     private void initialiseState()
     {
-        totalQuantity = 0;
-    }
-    
-    @Override
-    public Integer addAppointment(AppointmentEntity appointmentEntity, Integer quantity)
-    {
-        totalQuantity += quantity;
         
-        return totalQuantity;
     }
     
-//    @Override
-//    public AppointmentEntity doBooking(Long customerId) 
-//    {
-//        AppointmentEntity newAppointmentEntity = appointmentEntitySessionBeanLocal.createNewAppointment(customerId, new AppointmentEntity());
-//        initialiseState();
-//        
-//        return newAppointmentEntity;
-//    }
+   // @Override
+   // public Integer addAppointment(AppointmentEntity appointmentEntity)
+   // {
+        
+   // }
     
     @Override
-    public void clearShoppingCart()
+    public Long doBooking(Long customerId, Long serviceProviderId) throws UnknownPersistenceException, InputDataValidationException, AppointmentNumberExistsException, CustomerNotFoundException, ServiceProviderNotFoundException, ServiceProviderBlockedException
+    {
+
+        Long newAppointmentEntityId = appointmentEntitySessionBeanLocal.createNewAppointment(customerId, serviceProviderId, new AppointmentEntity(appointmentNo, scheduledTime, scheduledDate, customerEntity, serviceProviderEntity, businessCategoryEntity));
+        
+        initialiseState();
+        return newAppointmentEntityId;
+    }
+    
+    @Override
+    public void clearAppointmentCart()
     {
         initialiseState();
     }
-    
+
     @Override
-    public Integer getTotalQuantity() {
-        return totalQuantity;
+    public String getAppointmentNo() {
+        return appointmentNo;
     }
 
     @Override
-    public void setTotalQuantity(Integer totalQuantity) {
-        this.totalQuantity = totalQuantity;
+    public void setAppointmentNo(String appointmentNo) {
+        this.appointmentNo = appointmentNo;
     }
 
     @Override
-    public AppointmentEntity doBooking(Long customerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public LocalTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    @Override
+    public void setScheduledTime(LocalTime scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+
+    @Override
+    public LocalDate getScheduledDate() {
+        return scheduledDate;
+    }
+
+    @Override
+    public void setScheduledDate(LocalDate scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+
+    @Override
+    public CustomerEntity getCustomerEntity() {
+        return customerEntity;
+    }
+
+    @Override
+    public void setCustomerEntity(CustomerEntity customerEntity) {
+        this.customerEntity = customerEntity;
+    }
+
+    @Override
+    public ServiceProviderEntity getServiceProviderEntity() {
+        return serviceProviderEntity;
+    }
+
+    @Override
+    public void setServiceProviderEntity(ServiceProviderEntity serviceProviderEntity) {
+        this.serviceProviderEntity = serviceProviderEntity;
+    }
+
+    @Override
+    public BusinessCategoryEntity getBusinessCategoryEntity() {
+        return businessCategoryEntity;
+    }
+
+    @Override
+    public void setBusinessCategoryEntity(BusinessCategoryEntity businessCategoryEntity) {
+        this.businessCategoryEntity = businessCategoryEntity;
     }
 }
--- EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBeanLocal.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWeb-ejb/src/java/ejb/session/stateful/BookingSessionBeanLocal.java
@@ -1,18 +1,48 @@
 package ejb.session.stateful;
 
 import entity.AppointmentEntity;
+import entity.BusinessCategoryEntity;
+import entity.CustomerEntity;
+import entity.ServiceProviderEntity;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import util.exception.AppointmentNumberExistsException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderBlockedException;
+import util.exception.ServiceProviderNotFoundException;
+import util.exception.UnknownPersistenceException;
 
 
 public interface BookingSessionBeanLocal {
+    
+    public Long doBooking(Long customerId, Long serviceProviderId) throws UnknownPersistenceException, InputDataValidationException, AppointmentNumberExistsException, CustomerNotFoundException, ServiceProviderNotFoundException, ServiceProviderBlockedException;
 
-    public Integer addAppointment(AppointmentEntity appointmentEntity, Integer quantity);
+    public void clearAppointmentCart();
 
-    public AppointmentEntity doBooking(Long customerId);
+    public String getAppointmentNo();
 
-    public void clearShoppingCart();
+    public void setAppointmentNo(String appointmentNo);
 
-    public Integer getTotalQuantity();
+    public LocalTime getScheduledTime();
 
-    public void setTotalQuantity(Integer totalQuantity);
+    public void setScheduledTime(LocalTime scheduledTime);
+
+    public LocalDate getScheduledDate();
+
+    public void setScheduledDate(LocalDate scheduledDate);
+
+    public CustomerEntity getCustomerEntity();
+
+    public void setCustomerEntity(CustomerEntity customerEntity);
+
+    public ServiceProviderEntity getServiceProviderEntity();
+
+    public void setServiceProviderEntity(ServiceProviderEntity serviceProviderEntity);
+
+    public BusinessCategoryEntity getBusinessCategoryEntity();
+
+    public void setBusinessCategoryEntity(BusinessCategoryEntity businessCategoryEntity);
+    
     
 }
