5df063166aea8ebb0e199381f26f4d8d56c04115
jieyingma <jieyingma@jieyings-mbp.lan>
2021-04-11 19:06:32 +0800

    commit bookingSB

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateful/BookingSessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateful/BookingSessionBeanRemote.java
@@ -1,17 +1,47 @@
 package ejb.session.stateful;
 
 import entity.AppointmentEntity;
+import entity.BusinessCategoryEntity;
+import entity.CustomerEntity;
+import entity.ServiceProviderEntity;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import util.exception.AppointmentNumberExistsException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.ServiceProviderBlockedException;
+import util.exception.ServiceProviderNotFoundException;
+import util.exception.UnknownPersistenceException;
 
 
 public interface BookingSessionBeanRemote {
     
-    public Integer addAppointment(AppointmentEntity appointmentEntity, Integer quantity);
+    public Long doBooking(Long customerId, Long serviceProviderId) throws UnknownPersistenceException, InputDataValidationException, AppointmentNumberExistsException, CustomerNotFoundException, ServiceProviderNotFoundException, ServiceProviderBlockedException;
 
-    public AppointmentEntity doBooking(Long customerId);
+    public void clearAppointmentCart();
 
-    public void clearShoppingCart();
+    public String getAppointmentNo();
 
-    public Integer getTotalQuantity();
+    public void setAppointmentNo(String appointmentNo);
 
-    public void setTotalQuantity(Integer totalQuantity);
+    public LocalTime getScheduledTime();
+
+    public void setScheduledTime(LocalTime scheduledTime);
+
+    public LocalDate getScheduledDate();
+
+    public void setScheduledDate(LocalDate scheduledDate);
+
+    public CustomerEntity getCustomerEntity();
+
+    public void setCustomerEntity(CustomerEntity customerEntity);
+
+    public ServiceProviderEntity getServiceProviderEntity();
+
+    public void setServiceProviderEntity(ServiceProviderEntity serviceProviderEntity);
+
+    public BusinessCategoryEntity getBusinessCategoryEntity();
+
+    public void setBusinessCategoryEntity(BusinessCategoryEntity businessCategoryEntity);
+    
 }
