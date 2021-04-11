4a11bbb13aa10fab010f25cb1e83f69c4911c186
jieyingma <jieyingma@jieyings-mbp.lan>
2021-04-11 17:27:35 +0800

    commit bizCatejb

--- EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
+++ EasyAppointmentSystemWeb/EasyAppointmentSystemWebLibrary/src/ejb/session/stateless/BusinessCategoryEntitySessionBeanRemote.java
@@ -17,8 +17,8 @@ public interface BusinessCategoryEntitySessionBeanRemote {
 
     public String createNewBusinessCategoryEntity(BusinessCategoryEntity newBusinessCategoryEntity) throws BusinessCategoryExistException, UnknownPersistenceException, InputDataValidationException;
     
-    public void deleteBusinessCategory(String category) throws DeleteBusinessCategoryException, BusinessCategoryNotFoundException;
-    
+    public void deleteBusinessCategory(Long businessCategoryId) throws DeleteBusinessCategoryException, BusinessCategoryNotFoundException;
+
     public BusinessCategoryEntity retrieveBusinessCategoriesByName(String name) throws BusinessCategoryNotFoundException;
     
     public BusinessCategoryEntity retrieveBusinessCategoriesById(Long businessCategoryId) throws BusinessCategoryNotFoundException;
